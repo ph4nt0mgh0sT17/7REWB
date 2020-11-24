61dc943b5c735606e154e68a9e044a343f52bc89
David Miko <ph4nt0mgh0sT17@gmail.com>
2020-11-24 19:58:47 +0100

    New homework 04-11-20 + exercise

---
+++ scripts/mikrite-carousel.js
@@ -0,0 +1,20 @@
+$(function () {
+
+    /*var mouseEntered = false;
+
+    $("#gamesDropdownLink").mouseenter(function() {
+        $("div.dropdown-menu").addClass("show");
+    });
+
+    $("div.dropdown-menu").mouseenter(function() {
+        $("div.dropdown-menu").addClass("show");
+    });
+
+    $("#gamesDropdownLink").mouseleave(function() {
+        $("div.dropdown-menu").removeClass("show");
+    });
+
+    $("div.dropdown-menu").mouseleave(function() {
+        $("div.dropdown-menu").removeClass("show");
+    });*/
+});
